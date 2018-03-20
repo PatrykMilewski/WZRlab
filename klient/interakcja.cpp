09a572393269c6815d2d1c608cc9e40ca8d46516
Dominik Kinal <d.kinal@rabbitgames.pl>
2018-03-20 21:26:09 +0100

    Update interakcja.cpp

--- klient/interakcja.cpp
+++ klient/interakcja.cpp
@@ -1,6 +1,6 @@
 /************************************************************
 Interakcja:
-Wysy³anie, odbiór komunikatów, interakcja z innymi
+WysyÂ³anie, odbiÃ³r komunikatÃ³w, interakcja z innymi
 uczestnikami WZR, sterowanie wirtualnymi obiektami
 *************************************************************/
 
@@ -29,27 +29,28 @@ unicast_net *multi_reciv;         // wsk do obiektu zajmujacego sie odbiorem kom
 unicast_net *multi_send;          //   -||-  wysylaniem komunikatow
 
 unsigned long przychodzacy_adres_IP = 0;
-unsigned long serwera_adres_IP = inet_addr("153.19.217.33");
+char *adres_IP = "192.168.0.10";
+unsigned long serwera_adres_IP = inet_addr(adres_IP);
 
-HANDLE threadReciv;                 // uchwyt w¹tku odbioru komunikatów
+HANDLE threadReciv;                 // uchwyt wÂ¹tku odbioru komunikatÃ³w
 extern HWND okno;
 int SHIFTwcisniety = 0;
-bool czy_rysowac_ID = 1;            // czy rysowac nr ID przy ka¿dym obiekcie
-bool sterowanie_myszkowe = 0;       // sterowanie za pomoc¹ klawisza myszki
-int kursor_x = 0, kursor_y = 0;     // po³o¿enie kursora myszy
+bool czy_rysowac_ID = 1;            // czy rysowac nr ID przy kaÂ¿dym obiekcie
+bool sterowanie_myszkowe = 0;       // sterowanie za pomocÂ¹ klawisza myszki
+int kursor_x = 0, kursor_y = 0;     // poÂ³oÂ¿enie kursora myszy
 
 // Parametry widoku:
 Wektor3 kierunek_kamery_1 = Wektor3(11, -3, -14),   // kierunek patrzenia
-pol_kamery_1 = Wektor3(-34, 6, 10),         // po³o¿enie kamery
+pol_kamery_1 = Wektor3(-34, 6, 10),         // poÂ³oÂ¿enie kamery
 pion_kamery_1 = Wektor3(0, 1, 0),           // kierunek pionu kamery        
-kierunek_kamery_2 = Wektor3(0, -1, 0.02),   // to samo dla widoku z góry
+kierunek_kamery_2 = Wektor3(0, -1, 0.02),   // to samo dla widoku z gÃ³ry
 pol_kamery_2 = Wektor3(0, 90, 0),
 pion_kamery_2 = Wektor3(0, 0, -1),
 kierunek_kamery = kierunek_kamery_1, pol_kamery = pol_kamery_1, pion_kamery = pion_kamery_1;
-bool sledzenie = 1;                             // tryb œledzenia obiektu przez kamerê
-bool widok_z_gory = 0;                          // tryb widoku z góry
+bool sledzenie = 1;                             // tryb Å“ledzenia obiektu przez kamerÃª
+bool widok_z_gory = 0;                          // tryb widoku z gÃ³ry
 float oddalenie = 36.0;                          // oddalenie widoku z kamery
-float kat_kam_z = 0;                            // obrót kamery góra-dó³
+float kat_kam_z = 0;                            // obrÃ³t kamery gÃ³ra-dÃ³Â³
 float oddalenie_1 = oddalenie, kat_kam_z_1 = kat_kam_z, oddalenie_2 = oddalenie, kat_kam_z_2 = kat_kam_z,
 oddalenie_3 = oddalenie, kat_kam_z_3 = kat_kam_z;
 
@@ -57,7 +58,7 @@ oddalenie_3 = oddalenie, kat_kam_z_3 = kat_kam_z;
 enum typy_ramek{ STAN_OBIEKTU, INFO_O_ZAMKNIECIU, OFERTA };
 
 // rozbudowac, dodac info czy aplikacja zostala zamknieta przez uzytkownika
-struct Ramka                                    // g³ówna struktura s³u¿¹ca do przesy³ania informacji
+struct Ramka                                    // gÂ³Ã³wna struktura sÂ³uÂ¿Â¹ca do przesyÂ³ania informacji
 {
 	int typ;
 	StanObiektu stan;
@@ -66,30 +67,30 @@ struct Ramka                                    // g³ówna struktura s³u¿¹ca do p
 
 
 //******************************************
-// Funkcja obs³ugi w¹tku odbioru komunikatów 
+// Funkcja obsÂ³ugi wÂ¹tku odbioru komunikatÃ³w 
 DWORD WINAPI WatekOdbioru(void *ptr)
 {
-	unicast_net *pu_net = (unicast_net*)ptr;                // wskaŸnik do obiektu klasy unicast_net
-	int rozmiar;                                               // liczba bajtów ramki otrzymanej z sieci
+	unicast_net *pu_net = (unicast_net*)ptr;                // wskaÅ¸nik do obiektu klasy unicast_net
+	int rozmiar;                                               // liczba bajtÃ³w ramki otrzymanej z sieci
 	Ramka ramka;
 	StanObiektu stan;
 
 	while (1)
 	{
-		rozmiar = pu_net->reciv((char*)&ramka, &przychodzacy_adres_IP, sizeof(Ramka));   // oczekiwanie na nadejœcie ramki - funkcja samoblokuj¹ca siê 
+		rozmiar = pu_net->reciv((char*)&ramka, &przychodzacy_adres_IP, sizeof(Ramka));   // oczekiwanie na nadejÅ“cie ramki - funkcja samoblokujÂ¹ca siÃª 
 		switch (ramka.typ)
 		{
 		case STAN_OBIEKTU:
 		{
 			stan = ramka.stan;
 
-			if (stan.iID != pMojObiekt->iID)                     // jeœli to nie mój obiekt
+			if (stan.iID != pMojObiekt->iID)                     // jeÅ“li to nie mÃ³j obiekt
 			{
-				if (IndeksyOb[stan.iID] == -1)                     // nie ma jeszcze takiego obiektu w tablicy -> trzeba go stworzyæ
+				if (IndeksyOb[stan.iID] == -1)                     // nie ma jeszcze takiego obiektu w tablicy -> trzeba go stworzyÃ¦
 				{
 					CudzeObiekty[iLiczbaCudzychOb] = new ObiektRuchomy(&teren);
 					IndeksyOb[stan.iID] = iLiczbaCudzychOb;          // wpis do tablicy indeksowanej numerami ID
-					// u³atwia wyszukiwanie, alternatyw¹ mo¿e byæ tabl. rozproszona                                                                                               
+					// uÂ³atwia wyszukiwanie, alternatywÂ¹ moÂ¿e byÃ¦ tabl. rozproszona                                                                                               
 					iLiczbaCudzychOb++;
 				}
 				CudzeObiekty[IndeksyOb[stan.iID]]->ZmienStan(stan);   // zmiana stanu obiektu obcego 			
@@ -100,7 +101,7 @@ DWORD WINAPI WatekOdbioru(void *ptr)
 		{
 			break;
 		}
-		case INFO_O_ZAMKNIECIU:          // zwyk³y komunikat tekstowy
+		case INFO_O_ZAMKNIECIU:          // zwykÂ³y komunikat tekstowy
 		{
 			stan = ramka.stan;
 			CudzeObiekty[IndeksyOb[stan.iID]] = nullptr;
@@ -115,8 +116,8 @@ DWORD WINAPI WatekOdbioru(void *ptr)
 }
 
 // *****************************************************************
-// ****    Wszystko co trzeba zrobiæ podczas uruchamiania aplikacji
-// ****    poza grafik¹   
+// ****    Wszystko co trzeba zrobiÃ¦ podczas uruchamiania aplikacji
+// ****    poza grafikÂ¹   
 void PoczatekInterakcji()
 {
 	DWORD dwThreadId;
@@ -130,7 +131,7 @@ void PoczatekInterakcji()
 
 	// obiekty sieciowe typu unicast (z podaniem adresu WZR oraz numeru portu)
 	multi_reciv = new unicast_net(10002);      // obiekt do odbioru ramek sieciowych
-	multi_send = new unicast_net(10001);       // obiekt do wysy³ania ramek
+	multi_send = new unicast_net(10001);       // obiekt do wysyÂ³ania ramek
 
 
 	// uruchomienie watku obslugujacego odbior komunikatow
@@ -146,52 +147,52 @@ void PoczatekInterakcji()
 
 
 // *****************************************************************
-// ****    Wszystko co trzeba zrobiæ w ka¿dym cyklu dzia³ania 
-// ****    aplikacji poza grafik¹ 
+// ****    Wszystko co trzeba zrobiÃ¦ w kaÂ¿dym cyklu dziaÂ³ania 
+// ****    aplikacji poza grafikÂ¹ 
 void Cykl_WS()
 {
 	licznik_sym++;
 
 	// obliczenie czasu fDt pomiedzy dwoma kolejnymi cyklami
-	if (licznik_sym % 70 == 0)          // jeœli licznik cykli przekroczy³ pewn¹ wartoœæ, to
-	{                                   // nale¿y na nowo obliczyæ œredni czas cyklu fDt
+	if (licznik_sym % 70 == 0)          // jeÅ“li licznik cykli przekroczyÂ³ pewnÂ¹ wartoÅ“Ã¦, to
+	{                                   // naleÂ¿y na nowo obliczyÃ¦ Å“redni czas cyklu fDt
 		char text[256];
 		long czas_pop = czas_cyklu_WS;
 		czas_cyklu_WS = clock();
 		float fFps = (70 * CLOCKS_PER_SEC) / (float)(czas_cyklu_WS - czas_pop);
 		if (fFps != 0) fDt = 1.0 / fFps; else fDt = 1;
 		sprintf(text, "WZR-lab 2018 temat 1, wersja h, (%0.0f fps  %0.2fms)  ", fFps, 1000.0 / fFps);
-		SetWindowText(okno, text); // wyœwietlenie aktualnej iloœci klatek/s w pasku okna			
+		SetWindowText(okno, text); // wyÅ“wietlenie aktualnej iloÅ“ci klatek/s w pasku okna			
 	}
 
 
-	pMojObiekt->Symulacja(fDt);                    // symulacja obiektu w³asnego 
+	pMojObiekt->Symulacja(fDt);                    // symulacja obiektu wÂ³asnego 
 
 	Ramka ramka;
 	ramka.typ = STAN_OBIEKTU;
-	ramka.stan = pMojObiekt->Stan();               // stan w³asnego obiektu 
+	ramka.stan = pMojObiekt->Stan();               // stan wÂ³asnego obiektu 
 
 
-	// wys³anie komunikatu o stanie obiektu przypisanego do aplikacji (pMojObiekt):    
+	// wysÂ³anie komunikatu o stanie obiektu przypisanego do aplikacji (pMojObiekt):    
 	multi_send->send((char*)&ramka, serwera_adres_IP, sizeof(Ramka));
 }
 
 // *****************************************************************
-// ****    Wszystko co trzeba zrobiæ podczas zamykania aplikacji
-// ****    poza grafik¹ 
+// ****    Wszystko co trzeba zrobiÃ¦ podczas zamykania aplikacji
+// ****    poza grafikÂ¹ 
 void ZakonczenieInterakcji()
 {
-	fprintf(f, "Interakcja zosta³a zakoñczona\n");
+	fprintf(f, "Interakcja zostaÂ³a zakoÃ±czona\n");
 	Ramka ramka;
 	ramka.typ = INFO_O_ZAMKNIECIU;
 	ramka.stan = pMojObiekt->Stan();
-	multi_send->send((char*)&ramka, "153.19.217.33", sizeof(Ramka));
+	multi_send->send((char*)&ramka, adres_IP, sizeof(Ramka));
 	fclose(f);
 }
 
 
 // ************************************************************************
-// ****    Obs³uga klawiszy s³u¿¹cych do sterowania obiektami lub
+// ****    ObsÂ³uga klawiszy sÂ³uÂ¿Â¹cych do sterowania obiektami lub
 // ****    widokami 
 void KlawiszologiaSterowania(UINT kod_meldunku, WPARAM wParam, LPARAM lParam)
 {
@@ -204,7 +205,7 @@ void KlawiszologiaSterowania(UINT kod_meldunku, WPARAM wParam, LPARAM lParam)
 		int x = LOWORD(lParam);
 		int y = HIWORD(lParam);
 		if (sterowanie_myszkowe)
-			pMojObiekt->F = 40.0;        // si³a pchaj¹ca do przodu
+			pMojObiekt->F = 40.0;        // siÂ³a pchajÂ¹ca do przodu
 		break;
 	}
 	case WM_RBUTTONDOWN: //reakcja na prawy przycisk myszki
@@ -212,11 +213,11 @@ void KlawiszologiaSterowania(UINT kod_meldunku, WPARAM wParam, LPARAM lParam)
 		int x = LOWORD(lParam);
 		int y = HIWORD(lParam);
 		if (sterowanie_myszkowe)
-			pMojObiekt->F = -8.0;        // si³a pchaj¹ca do tylu
+			pMojObiekt->F = -8.0;        // siÂ³a pchajÂ¹ca do tylu
 
 		break;
 	}
-	case WM_MBUTTONDOWN: //reakcja na œrodkowy przycisk myszki : uaktywnienie/dezaktywacja sterwania myszkowego
+	case WM_MBUTTONDOWN: //reakcja na Å“rodkowy przycisk myszki : uaktywnienie/dezaktywacja sterwania myszkowego
 	{
 		sterowanie_myszkowe = 1 - sterowanie_myszkowe;
 		kursor_x = LOWORD(lParam);
@@ -226,13 +227,13 @@ void KlawiszologiaSterowania(UINT kod_meldunku, WPARAM wParam, LPARAM lParam)
 	case WM_LBUTTONUP: //reakcja na puszczenie lewego przycisku myszki
 	{
 		if (sterowanie_myszkowe)
-			pMojObiekt->F = 0.0;        // si³a pchaj¹ca do przodu
+			pMojObiekt->F = 0.0;        // siÂ³a pchajÂ¹ca do przodu
 		break;
 	}
 	case WM_RBUTTONUP: //reakcja na puszczenie lewy przycisk myszki
 	{
 		if (sterowanie_myszkowe)
-			pMojObiekt->F = 0.0;        // si³a pchaj¹ca do przodu
+			pMojObiekt->F = 0.0;        // siÂ³a pchajÂ¹ca do przodu
 		break;
 	}
 	case WM_MOUSEMOVE:
@@ -260,13 +261,13 @@ void KlawiszologiaSterowania(UINT kod_meldunku, WPARAM wParam, LPARAM lParam)
 		}
 		case VK_SPACE:
 		{
-			pMojObiekt->ham = 10.0;       // stopieñ hamowania (reszta zale¿y od si³y docisku i wsp. tarcia)
-			break;                       // 1.0 to maksymalny stopieñ (np. zablokowanie kó³)
+			pMojObiekt->ham = 10.0;       // stopieÃ± hamowania (reszta zaleÂ¿y od siÂ³y docisku i wsp. tarcia)
+			break;                       // 1.0 to maksymalny stopieÃ± (np. zablokowanie kÃ³Â³)
 		}
 		case VK_UP:
 		{
 
-			pMojObiekt->F = 50.0;        // si³a pchaj¹ca do przodu
+			pMojObiekt->F = 50.0;        // siÂ³a pchajÂ¹ca do przodu
 			break;
 		}
 		case VK_DOWN:
@@ -299,26 +300,26 @@ void KlawiszologiaSterowania(UINT kod_meldunku, WPARAM wParam, LPARAM lParam)
 			else oddalenie = 0;
 			break;
 		}
-		case 'S':   // przybli¿enie widoku
+		case 'S':   // przybliÂ¿enie widoku
 		{
 			//pol_kamery = pol_kamery + kierunek_kamery*0.3; 
 			if (oddalenie > 0) oddalenie *= 1.2;
 			else oddalenie = 0.5;
 			break;
 		}
-		case 'Q':   // widok z góry
+		case 'Q':   // widok z gÃ³ry
 		{
 			//if (sledzenie) break;
 			widok_z_gory = 1 - widok_z_gory;
-			if (widok_z_gory)                // przechodzimy do widoku z góry
+			if (widok_z_gory)                // przechodzimy do widoku z gÃ³ry
 			{
 				pol_kamery_1 = pol_kamery; kierunek_kamery_1 = kierunek_kamery; pion_kamery_1 = pion_kamery;
 				oddalenie_1 = oddalenie; kat_kam_z_1 = kat_kam_z;
 
 				//pol_kamery = pol_kamery_2; kierunek_kamery = kierunek_kamery_2; pion_kamery = pion_kamery_2;
 				oddalenie = oddalenie_2; kat_kam_z = kat_kam_z_2;
-				// Po³o¿enie kamery, kierunek oraz pion ustawiamy tak, by obiekt widziany by³ z góry i jecha³
-				// pocz¹tkowo w górê ekranu:
+				// PoÂ³oÂ¿enie kamery, kierunek oraz pion ustawiamy tak, by obiekt widziany byÂ³ z gÃ³ry i jechaÂ³
+				// poczÂ¹tkowo w gÃ³rÃª ekranu:
 				kierunek_kamery = (pMojObiekt->wPol - teren.srodek).znorm()*(-1);
 				pol_kamery = pMojObiekt->wPol - kierunek_kamery*pMojObiekt->dlugosc * 10;
 				pion_kamery = pMojObiekt->qOrient.obroc_wektor(Wektor3(1, 0, 0));
@@ -328,8 +329,8 @@ void KlawiszologiaSterowania(UINT kod_meldunku, WPARAM wParam, LPARAM lParam)
 				pol_kamery_2 = pol_kamery; kierunek_kamery_2 = kierunek_kamery; pion_kamery_2 = pion_kamery;
 				oddalenie_2 = oddalenie; kat_kam_z_2 = kat_kam_z;
 
-				// Po³o¿enie kamery, kierunek oraz pion ustawiamy tak, by obiekt widziany by³ z prawego boku i jecha³
-				// pocz¹tkowo ze strony lewej na praw¹:
+				// PoÂ³oÂ¿enie kamery, kierunek oraz pion ustawiamy tak, by obiekt widziany byÂ³ z prawego boku i jechaÂ³
+				// poczÂ¹tkowo ze strony lewej na prawÂ¹:
 				kierunek_kamery = pMojObiekt->qOrient.obroc_wektor(Wektor3(0, 0, 1))*-1;
 				pol_kamery = pMojObiekt->wPol - kierunek_kamery*pMojObiekt->dlugosc * 10;
 				pion_kamery = (pMojObiekt->wPol - teren.srodek).znorm();
@@ -339,17 +340,17 @@ void KlawiszologiaSterowania(UINT kod_meldunku, WPARAM wParam, LPARAM lParam)
 			}
 			break;
 		}
-		case 'E':   // obrót kamery ku górze (wzglêdem lokalnej osi z)
+		case 'E':   // obrÃ³t kamery ku gÃ³rze (wzglÃªdem lokalnej osi z)
 		{
 			kat_kam_z += PI * 5 / 180;
 			break;
 		}
-		case 'D':   // obrót kamery ku do³owi (wzglêdem lokalnej osi z)
+		case 'D':   // obrÃ³t kamery ku doÂ³owi (wzglÃªdem lokalnej osi z)
 		{
 			kat_kam_z -= PI * 5 / 180;
 			break;
 		}
-		case 'A':   // w³¹czanie, wy³¹czanie trybu œledzenia obiektu
+		case 'A':   // wÂ³Â¹czanie, wyÂ³Â¹czanie trybu Å“ledzenia obiektu
 		{
 			sledzenie = 1 - sledzenie;
 			if (sledzenie)
